49075fadadbaaf06e4a988137e35181b88d740d8
alex-costich <alejandro.costich@edu.uag.mx>
2024-07-31 16:58:18 -0700

    Added index route

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,10 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import ErrorPage from './ErrorPage'
 import Contact from './routes/contact/Contact'
 import { contactLoader } from './routes/contact/contactLoader'
+import { destroyAction } from './routes/destroy-contact/destroyContact'
 import EditContact from './routes/edit-contact/Edit'
 import { editAction } from './routes/edit-contact/editAction'
+import Index from './routes/index/Index'
 import Root from './routes/root/Root'
 import { rootAction } from './routes/root/rootAction'
 import { rootLoader } from './routes/root/rootLoader'
@@ -24,6 +26,11 @@ const router = createBrowserRouter([
                 Whatever components are added as children will render
                 inside the parent component (in this case, Root).
             */
+			{ index: true, element: <Index /> },
+			/*
+				index tells the router to match and render this route when the user
+				is at the parent route's exact path
+			*/
 			{
 				path: 'contacts/:contactId', // :contactId is the dynamic segment, and will be accessed through URL Params
 				element: <Contact />,
@@ -34,10 +41,16 @@ const router = createBrowserRouter([
 				element: <EditContact />,
 				loader: contactLoader, // Loaders should not be shared between routes, we're just being lazy
 				action: editAction
+			},
+			{
+				path: 'contacts/:contactId/destroy',
+				errorElement: <div>Oops! There was an error.</div>,
+				action: destroyAction
 			}
 		]
 	}
 ])
+
 const container = document.getElementById('root')
 
 createRoot(container).render(
