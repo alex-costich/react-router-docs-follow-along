577af188d0d6ea616af1e182603a846645e512eb
alex-costich <alejandro.costich@edu.uag.mx>
2024-07-31 17:00:13 -0700

    Made search client-side

--- src/routes/contact/contactLoader.js
+++ src/routes/contact/contactLoader.js
@@ -2,5 +2,5 @@ import { getContact } from '../../contacts'
 
 export const contactLoader = async ({ params }) => {
 	const contact = await getContact(params.contactId)
-	return { contact }
+	return { contact } // response to request
 }
