55f509417d590af895ced9431ed35cbe90a5530a
alex-costich <alejandro.costich@edu.uag.mx>
2024-08-01 11:12:21 -0700

    Implemented optimistic UI strategy

--- src/routes/contact/Favorite.jsx
+++ src/routes/contact/Favorite.jsx
@@ -2,7 +2,15 @@ import { useFetcher } from 'react-router-dom'
 
 const Favorite = ({ favorite }) => {
 	const fetcher = useFetcher()
-	/* Use fetcher hook allows us to communicate with loaders and actions without causing a navigation. */
+		? fetcher.formData.get('favorite') === 'true'
+		: favorite
+	/* 
+	Use fetcher hook allows us to communicate with loaders and actions without causing a navigation.
+
+	The fetcher knows the form data being submitted to the action, so it's available to you on fetcher.formData.
+	This is used to immediately update the star's state, even though the network hasn't finished.
+	If the update eventually fails, the UI will revert to the real data.
+	*/
 
 	return (
 		<fetcher.Form method='post'>
