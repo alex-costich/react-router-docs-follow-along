e23ffab94d3452c4286dd4014e91be709b019977
alex-costich <alejandro.costich@edu.uag.mx>
2024-08-01 11:10:39 -0700

    Deleted a comment

--- src/routes/root/rootLoader.js
+++ src/routes/root/rootLoader.js
@@ -1,8 +1,16 @@
 import { getContacts } from '../../contacts'
 
 export const rootLoader = async ({ request }) => {
-	const url = new URL(request.url)
+	console.log(request)
+	const url = new URL(request.url) // Creates URL type object from request url
 	const q = url.searchParams.get('q')
-	const contacts = await getContacts(q)
-	return { contacts, q } // response to request
+	/* 
+	Retrieves the value of the query parameter named 'q'
+	from the URL. Query parameters are preceded by an interrogation mark (?)
+
+	For example, here: http://localhost:5173/?q=this+is+a+typed+input
+	The q const would be "this is a typed input"
+	*/
+	const contacts = await getContacts(q) // Looks for contacts named 'q'
+	return { contacts, q } // Response to request: matching contacts, query
 }
