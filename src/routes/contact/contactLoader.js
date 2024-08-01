aa3a99c758a8f1dc099d1d1f2a6f5efd33d70b6c
alex-costich <alejandro.costich@edu.uag.mx>
2024-08-01 13:47:52 -0700

    Added thrown error in case no contacts are loaded

--- src/routes/contact/contactLoader.js
+++ src/routes/contact/contactLoader.js
@@ -2,5 +2,16 @@ import { getContact } from '../../contacts'
 
 export const contactLoader = async ({ params }) => {
 	const contact = await getContact(params.contactId)
-	return { contact } // response to request
+	if (!contact) {
+		throw new Response('', {
+			status: 404,
+			statusText: 'Not Found'
+		})
+	}
+	/*
+	Whenever you have an expected error case in a loader or action–like the data not existing–you can throw.
+	The call stack will break, React Router will catch it, and the error path is rendered instead. We won't
+	even try to render a null contact.
+	*/
+	return { contact } // Response to request: returns contact
 }
