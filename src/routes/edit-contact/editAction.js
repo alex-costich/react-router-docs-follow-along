101a73c2b33d79eb24e7d273c05962951ece466c
alex-costich <alejandro.costich@edu.uag.mx>
2024-07-31 17:02:00 -0700

    Added a redirect and documentation

--- src/routes/edit-contact/editAction.js
+++ src/routes/edit-contact/editAction.js
@@ -3,7 +3,20 @@ import { updateContact } from '../../contacts'
 
 export const editAction = async ({ request, params }) => {
 	const formData = await request.formData()
-	const updates = Object.fromEntries(formData)
+	/* 
+	Without JS, formData is created and sent in the request when a form is submitted.
+	Because we are using Forms and not forms, they are sent to the route's action instead.
+	*/
+	const updates = Object.fromEntries(formData) // Collects all form fields into one object
+	/* 
+	They can also be accessed individually using formData.get(name)
+	Or through updates.name
+	*/
 	await updateContact(params.contactId, updates)
-	return redirect(`/contacts/${params.contactId}`)
+	return redirect(`/contacts/${params.contactId}`) // response to request
 }
+
+/*
+Aside from action, none of these APIs are provided by React Router: request,
+request.formData, Object.fromEntries are all provided by the web platform. 
+*/
